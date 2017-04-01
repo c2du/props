1393ca5eaededd7b304421b3698bebeb15a6e381
Connie Du <c2du@ucsd.edu>
2017-04-01 13:29:15 -0700

    Tested google sign in, points to mainActivity

--- app/src/main/java/com/lahacks/props/LoginActivity.java
+++ app/src/main/java/com/lahacks/props/LoginActivity.java
@@ -73,27 +73,28 @@ public class LoginActivity extends AppCompatActivity implements
         @Override
         public void onStart() {
             super.onStart();
-
-            OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-            if (opr.isDone()) {
-                // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-                // and the GoogleSignInResult will be available instantly.
-                Log.d(TAG, "Got cached sign-in");
-                GoogleSignInResult result = opr.get();
-                handleSignInResult(result);
-            } else {
-                // If the user has not previously signed in on this device or the sign-in has expired,
-                // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-                // single sign-on will occur in this branch.
-                showProgressDialog();
-                opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                    @Override
-                    public void onResult(GoogleSignInResult googleSignInResult) {
-                        hideProgressDialog();
-                        handleSignInResult(googleSignInResult);
-                    }
-                });
-            }
+//
+//
+//            OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+//            if (opr.isDone()) {
+//                // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+//                // and the GoogleSignInResult will be available instantly.
+//                Log.d(TAG, "Got cached sign-in");
+//                GoogleSignInResult result = opr.get();
+//                handleSignInResult(result);
+//            } else {
+//                // If the user has not previously signed in on this device or the sign-in has expired,
+//                // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+//                // single sign-on will occur in this branch.
+//                showProgressDialog();
+//                opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+//                    @Override
+//                    public void onResult(GoogleSignInResult googleSignInResult) {
+//                        hideProgressDialog();
+//                        handleSignInResult(googleSignInResult);
+//                    }
+//                });
+//            }
         }
 
         // [START onActivityResult]
@@ -111,14 +112,19 @@ public class LoginActivity extends AppCompatActivity implements
 
         // [START handleSignInResult]
         private void handleSignInResult(GoogleSignInResult result) {
-            Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+            Log.d(TAG, "handleSignInResult:" + result.toString() + " | " + result.getStatus() + " | ");
             if (result.isSuccess()) {
                 // Signed in successfully, show authenticated UI.
                 GoogleSignInAccount acct = result.getSignInAccount();
                // mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+
                 updateUI(true);
+                Intent mainIntent = new Intent(this, MainActivity.class);
+                startActivity(mainIntent);
+                Log.d("login", "successful login");
             } else {
                 // Signed out, show unauthenticated UI.
+                Log.d("login", "unsuccessful login");
                 updateUI(false);
             }
         }
